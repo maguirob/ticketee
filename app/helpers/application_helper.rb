8c1c3446e8fa119ad539dc1ff1823b91cf537011
Bob Maguire <bibo85@yahoo.com>
2014-03-30 09:55:17 -0400

    Add title functionality for show page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+   def title(*parts)
+   	 unless parts.empty?
+   	   content_for :title do |variable|
+   	   	 (parts << "Ticketee").join(" - ")
+   	   end
+   	 end
+   end	   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -46,4 +46,7 @@ Ticketee::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+  root "projects#index"
+
+  resources :projects
 end
